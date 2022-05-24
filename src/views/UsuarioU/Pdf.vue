563127a066c8dbe9992564b1ee67fc4aeae9ba67
andresmedinaskp <medina970521@gmail.com>
2022-05-23 19:12:57 -0500

    se modifica vista pdf

--- src/views/UsuarioU/Pdf.vue
+++ src/views/UsuarioU/Pdf.vue
@@ -1,41 +1,15 @@
 <template>
-    
+    <div class="cara2">
 
-<div class="cara2">
-
-        <div class="contenedor_todo">
-      <div class="contenedor1">
-        <img
-          src="@/assets/logo.png"
-          class="logo"
-          height="80px"
-          width="80px"
-          alt="Kitten"
-        />
-        <h1 class="titulo">Libreria Virtual</h1>
-      </div>
-      <div class="contenedor2">
-       
-      </div>
-    </div>
-
-    <div class="contenedor3">
-          <section>
-     
- 
-      <div class="archive" v-for="todo in todos" :key=todo.id>
-        
-          
-          <iframe class="archive" :src="`http://127.0.0.1:8000/storage/${todo.pdf}`"></iframe>
-          
-        
-      </div>
-      <br>
-      <button type="button" class="btn btn-dark margen" v-on:click="downloadWithAxios('','material' )">Descargar</button>
+      <section>
+          <div class="archive" v-for="todo in todos" :key=todo.id>
+              <iframe class="archive" :src="`http://127.0.0.1:8000/storage/${todo.pdf}`"></iframe>
+          </div>
+          <br>
+          <button type="button" class="btn btn-dark margen" v-on:click="downloadWithAxios('','material' )">Descargar</button>
        </section>
     </div>
 
-  </div>
 </template>
 
 <script>
@@ -111,80 +85,13 @@ export default {
 </script>
 
 <style scoped>
-.contenedor_todo {
-  border: 1px solid black;
-  background: #16223f;
-  display: flex;
-  justify-content: space-between;
-}
-.contenedor1 {
-  /* border: 1px solid red; */
-  display: flex;
-  align-items: center;
-}
-.logo {
-  border: 1px blueviolet;
-  padding: 4px;
-}
-.titulo {
-  /* border: 1px solid red; */
-  color: white;
-  padding-left: 20px;
-}
-.contenedor2 {
-  /* border: 1px solid red; */
-  padding-top: 10px;
-  
-}
-.ingresar {
-  /* border: 1px solid black; */
-  color: white;
-  width: 80%;
-}
-
-.contenedor4 {
-  
-  margin: 100px;
-  display: flex;
-  justify-content: center;
-}
-.barra{
-  
-  font-size: 16px;
-  font-weight: 400;
-  line-height: 1.5;
-  display: block;
-  width: 70%;
-  padding: 10.5px 10px;
-  transition: all 0.2s;
-  color:#16223f;
-  border: 1px solid rgba(0, 0, 0, 0.442);
-  border-radius: 4px;
-  background-color: white;
-  background-clip: padding-box;
-}
-.boton{
-  border-color: #16223f;
-  border-radius: 4px;
-  color: white;
-  background-color: #16223f;
-  margin-left: 5px;
-}
-
-.contenedor3 {
-  /* border: 1px solid red; */
-  display: flex;
-  justify-content: center;
-  
-  margin: 30px;
-}
 .cara2{
     width: 100%;
-    
 }
 .archive{
-    width: 100vh;
-    height: 80vh;
+  margin-top: -20px;
+    width: 70vw;
+    height: 50vw;
     display: inline-block;
 }
 
