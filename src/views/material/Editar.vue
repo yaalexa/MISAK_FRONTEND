162f57a50c3f0b698c12b7a5c1eb40fd432ce6e1
wfernandosevilla677 <wfsevilla0@misena.edu.co>
2022-05-27 14:02:34 -0500

    editar material funcional

--- src/views/material/Editar.vue
+++ src/views/material/Editar.vue
@@ -8,191 +8,200 @@
             <div class="cara2">
             <section>  
               <div class="container  izquierda">
-               <h1>EDITAR MATERIAL</h1>  
-            
-                <form action="" class="form-horizontal">
+               <h1>EDITAR MATERIAL</h1> 
+
+<!--form-->
+
+
+                <form class="form-horizontal" action="">
                    <div class="col"></div>
                     <div class="form-group left">
                        <div class="col-sm-10">
                          <label for="" class="control-label col-sm-3">Nombre</label>
-                          <input type="text" class="form-control" name="name" id="name" v-model="form.name">
+                          <input type="text" class="form-control" name="name" id="name" v-model="mate.name">
                        </div>
                     </div>
                   <div class="form-group left row">
                       <div class="col">
                             <label for="" class="control-label col-sm-3">Año</label>
                             <div class="col-sm-7">
-                                <input type="text" class="form-control" name="year" id="year" v-model="form.year">
+                                <input type="text" class="form-control" name="year" id="year" v-model="mate.year">
                             </div>
                         </div>
                         <div class="col">
                           <label for="" class="control-label col-sm-5">ISBN</label>
                           <div class="col-sm-7">
-                             <input type="text" class="form-control" name="isbn" id="isbn" v-model="form.isbn">
+                             <input disabled type="text" class="form-control" name="isbn" id="isbn" v-model="mate.isbn">
                           </div>
                         </div> 
                     </div>
                     <div class="form-group left row">
                       <div class="col">
-                            <label for="" class="control-label col-sm-3"># Paginas</label>
+                            <label for="" class="control-label col-sm-3">#Paginas</label>
                             <div class="col-sm-7">
-                                <input type="text" class="form-control" name="num_pages" id="num_pages" v-model="form.num_pages">
+                                <input type="text" class="form-control" name="num_pages" id="num_pages" v-model="mate.num_pages">
                             </div>
                         </div>
-                        <div class="col">
-                          <label for="" class="control-label col-sm-5">PRIORIDAD</label>
-                          <div class="col-sm-7">
-                             <input type="text" class="form-control" name="priority" id="priority" v-model="form.priority">
-                          </div>
-                        </div> 
-                    </div>
-                   <div class="form-group left row">
-                      <div class="col">
-                            <label for="" class="control-label col-sm-3">PDF</label>
-                            <div class="col-sm-7">
-                                <input type="text" class="form-control" name="pdf" id="pdf" v-model="form.pdf">
-                            </div>
-                        </div>
-                        <div class="col">
-                          <label for="" class="control-label col-sm-5">IMAGEN</label>
-                          <div class="col-sm-7">
-                             <input type="text" class="form-control" name="img" id="img" v-model="form.img">
-                          </div>
-                        </div> 
-                    </div>
-                    <!--
-                    <div class="form-group left row">
-                      <div class="col">
-                            <label for="" class="control-label col-sm-3">USUARIO MATERIAL</label>
-                            <div class="col-sm-7">
-                                <input type="text" class="form-control" name="material_users_id" id="material_users_id" v-model="form.material_users_id">
-                            </div>
-                        </div>
-                        <div class="col">
-                          <label for="" class="control-label col-sm-5">ID TIPO DE MATERIAL</label>
-                          <div class="col-sm-7">
-                             <input type="text" class="form-control" name="type_material_id" id="type_material_id" v-model="form.type_material_id">
-                          </div>
-                        </div> 
-                    </div>
-                   
-                    <div class="form-group left row">
-                      <div class="col">
-                            <label for="" class="control-label col-sm-3">ID AUTOR</label>
-                            <div class="col-sm-7">
-                                <input type="text" class="form-control" name="author_books_id" id="author_books_id" v-model="form.author_books_id">
-                            </div>
-                        </div>
-                        <div class="col">
-                          <label for="" class="control-label col-sm-5">EDITORIAL ID</label>
-                          <div class="col-sm-7">
-                             <input type="text" class="form-control" name="editorial_id" id="editorial_id" v-model="form.editorial_id">
-                          </div>
-                        </div> 
-                    </div>
-                  <div class="form-group left row">
-                      <div class="col">
-                            <label for="" class="control-label col-sm-3">ID AREA</label>
-                            <div class="col-sm-7">
-                                <input type="text" class="form-control" name="area_id" id="area_id" v-model="form.area_id">
-                            </div>
-                        </div>
-                        <div class="col">
-                          <label for="" class="control-label col-sm-5">ID NIVELES DE EDUCACION</label>
-                          <div class="col-sm-7">
-                             <input type="text" class="form-control" name="material_educational_leves_id" id="material_educational_leves_id" v-model="form.material_educational_leves_id">
-                          </div>
-                        </div> 
-                    </div>
-                 
-  -->
-                   <br> 
-                 
-                      <button type="button" class="btn btn-primary" v-on:click="editar()" >Editar</button>
-                     
-                      <button type="button" class="btn btn-dark margen" v-on:click="salir()"  >Salir</button>
-                     
+
+                        <!--NUEVO EDITAR-->
+                                     <div class="col">
+			     <div class="col-xs-4">
+          	         <label class="firstname">Prioridad: </label></div>
+                        <div class="col-xs-8">
+                       <select name="document_type" id="document_type" class="form-control ">
+                             <br>
+                             <option value="1">Visualizado</option>
+                             <option value="2">Descargado</option>
+
+                             </select>   
+             </div>
+		      </div>
+<br>
+          <!--fin de ditar-->
+<!--para tipo material-->
+   <div class="form-group left row">
+        <div class="col">
+          <label for="" class="control-label col-sm-5">EDITORIAL</label>
+          <div class="col-sm-7">
+            <select class="form-control" v-model="mate.editorial_id">
+            <option v-for="editorial_id in  editorial_id" v-bind:value="editorial_id.id"> {{ editorial_id.name }}
+             </option>
+            </select>
+          </div>
+        </div>
+        <div class="col">
+          <label for="" class="control-label col-sm-3">AREA</label>
+          <div class="col-sm-7">
+           <select class="form-control" v-model="mate.area_id">
+            <option v-for="area_id in area_id" v-bind:value="area_id.id"> {{ area_id.name }}
+             </option>
+            </select>
+          </div>
+        </div>
+        </div>
+                       <div class="form-group left row">
+        <div class="col">
+          <label for="" class="control-label col-sm-5">TIPO MATERIAL</label>
+          <div class="col-sm-7">
+            <select class="form-control" v-model="mate.type_material_id">
+            <option v-for="type_material_id in type_material_id" v-bind:value="type_material_id.id"> {{ type_material_id.name }}
+             </option>
+            </select>
+          </div>
+        </div>
+        <br><br>
+              <div class="form-group">
+<br>
+        <button type="button" v-on:click="actualizar()" class="btn btn-primary">Actualizar</button>
+                <button type="button"  class="btn btn-dark margen" v-on:click="salir()">Salir</button>
+
+</div>        
+        </div> 
+<!--fin de tipo material-->
+
+</div>
+                   <br>                                           
                  
                 </form>
+<!--fin de form-->
+
                 </div>
             </section>
             </div>
-          <!-- <Footer />   -->
         </div>
     
 </template>
+
 <script>
 import Header from '@/components/Header.vue';
-//import Footer from '@/components/Footer.vue';
 import axios from 'axios';
-export default {
-  name:"Editar",
+export default{
+    name:"Edit",
   components:{
     Header,
     //Footer
   },
   data:function(){
-    return {
-        form:{
-          "name":"",
+      return{
+                          area_id:null,
+                                  type_material_id:null,
+editorial_id:null,
+          mate:{
+        "id":"",
+         "name":"",
           "isbn" : "",
           "year": "", 
           "num_pages" : "",
           "priority":"",
           "pdf" :"",
-          "img" : ""     
-        }
-    }
-  },
-  methods:{
-      editar(){
-          axios.put(`http://localhost:8000/api/materials/${this.form.materialid}`,this.form)
-          .then(response=>{
-              console.log(response);
-              this.$router.push("/dashboard");
-          })
-      },
-      salir(){
-        this.$router.push("/dashboard");
-      },
-      eliminar(){
-        var enviar = {
-            "materialid" : this.form.materialid,
-          
-        };
-        axios.delete("http://localhost:8000/api/materials", { headers : enviar})
-        .then( datos => {
-            console.log(datos);
-           this.$router.push("/dashboard");
-        });
-
+          "img" : "" ,
+        "editorial_id":"",
+        "area_id":"",
+        "type_material_id":""
+       
+             }
       }
   },
-  mounted:function(){
-      this.form.materialid = this.$route.params.id;
-      axios.get(`http://localhost:8000/api/materials/${this.form.materialid}`)
-      .then( datos => {
-        
-        this.form.name = datos.data[0].name;
-        this.form.isbn = datos.data[0].isbn;
-        this.form.year = datos.data[0].year;
-        this.form.num_pages = datos.data[0].num_pages;
-        this.form.priority = datos.data[0].priority;
-        this.form.num_pages = datos.data[0].num_pages;
-        this.form.priority = datos.data[0].priority;
-        this.form.pdf = datos.data[0].pdf;
-         this.form.img = datos.data[0].img;
-        this.form.material_users_id = datos.data[0].material_users_id;
-        this.form.type_material_id = datos.data[0].type_material_id;
-        this.form.author_books_id = datos.data[0].author_books_id;
-        this.form.editorial_id = datos.data[0].editorial_id;
-        this.form.area_id = datos.data[0].area_id;
-        this.form.material_educational_leves_id = datos.data[0].material_educational_leves_id;
-        console.log(this.form);
+  mounted(){
+this.mostrarForm();
+this.tipoMaterial();
+this.area();
+this.edito();
+  },
 
+  methods: {
+      tipoMaterial(){
+      let direccion = "http://localhost:8000/api/type_materials";
+        axios.get(direccion).then((result) => {
+        this.type_material_id = result.data;
+        });
+      },
+
+      area(){
+                  let direccion3 = "http://127.0.0.1:8000/api/areas";
+        axios.get(direccion3).then((result) => {
+        this.area_id = result.data;
+        });
+      },
+      edito(){
+                  let direccion2 = "http://127.0.0.1:8000/api/editorials";
+        axios.get(direccion2).then((result) => {
+        this.editorial_id = result.data;
+        
+        });
+      },
+      salir(){
+                    this.$router.push("/dashboard");
+
+      },
+
+
+
+
+      //cosas
+      actualizar(){
+          axios.put("http://127.0.0.1:8000/api/materials/"+ this.mate.id,this.mate).then(response=>{
+              console.log(response);
+                        this.$router.push("/dashboard");
+
+          })
+      },
+     async mostrarForm(){
+                this.mate.id=this.$route.params.id;
+      await axios.get("http://127.0.0.1:8000/api/materials/"+ this.mate.id).then(response=>{
+          this.mate.id=response.data[0].id;
+          this.mate.name=response.data[0].name;
+          this.mate.isbn=response.data[0].isbn;
+          this.mate.year=response.data[0].year;
+          this.mate.num_pages=response.data[0].num_pages;
+          this.mate.priority=response.data[0].priority;
+          this.mate.editorial_id=response.data[0].editorial_id;
+          this.mate.area_id=response.data[0].area_id;
+          this.mate.type_material_id=response.data[0].type_material_id;
+console.log(this.form);
       })
-     
-  }  
+      }  
+  },
 }
 </script>
 <style scoped>
